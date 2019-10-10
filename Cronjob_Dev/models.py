989e499cf4447763c4e082c9b8dd65cbf5947100
emanuele <emanuele.canzian@csbe.ch>
2019-10-10 11:40:05 +0200

    commit nachricht

--- Cronjob_Dev/models.py
+++ Cronjob_Dev/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class CronJob(models.Model):
+    title = models.CharField(max_length=128, null=False, default='')
+
+    url = models.URLField(max_length=255, null=False, default='')
+
+    auth = models.BooleanField(default=False)
+
+    username = models.CharField(max_length=30, null=False, default='')
+
+    password = models.CharField(max_length=30, null=False, default='')
+
+    minute = models.CharField(max_length=30, null=False, default='')
+
+    hour = models.CharField(max_length=30, null=False, default='')
+
+    dayOfMonth = models.CharField(max_length=30, null=False, default='')
+
+    month = models.CharField(max_length=30, null=False, default='')
+
+    weekday = models.CharField(max_length=30, null=False, default='')
--- Cronjob_Dev/views.py
+++ Cronjob_Dev/views.py
@@ -1,3 +1,116 @@
-from django.shortcuts import render
+from pyexpat.errors import messages
+
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from passlib.hash import pbkdf2_sha256
+from Cronjob_Dev.models import CronJob
+from .forms import SignUpForm
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'Cronjob_Dev/index.html', {})
+
+
+# login
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in.')
+            return redirect('cron')
+        else:
+            messages.error(request, 'Incorrect Info')
+            return redirect('login')
+    else:
+        return render(request, 'Cronjob_Dev/login_user.html', {})
+
+
+def cron(request):
+    return render(request, 'Cronjob_Dev/cron.html')
+
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'u have been registet')
+            return redirect('cron')
+
+    else:
+        form = SignUpForm()
+
+    context = {'form': form}
+    return render(request, 'Cronjob_Dev/register_user.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logout')
+    return redirect('index')
+
+
+def cron(request):
+    if request.method == 'POST':
+        tablefill = CronJob()
+        title = request.POST.get('title')
+        url = request.POST.get('url')
+
+        auth = request.POST.get('auth')
+        if auth == 'on':
+            auth = True
+        if auth is None:
+            auth = False
+
+        username = request.POST.get('username')
+
+        password = request.POST.get('password')
+        if password != '':
+            enc_password = pbkdf2_sha256.encrypt(password, rounds="12000", salt_size=32)
+        else:
+            enc_password = ''
+
+        if auth == 'on' and username == '' or password == '':
+            messages.error(request, 'wen http aktiviert ist müssen sie einen benutzer und Passwort eingeben')
+            return render(request, 'Cronjob_Dev/cron.html')
+
+        auswahl = request.POST.get('ausführung')
+        if auswahl == '1':
+            minute = request.POST.get('mins')
+            minute = tablefill.minute
+        if auswahl == '2':
+            hour = request.POST.get('hours')
+            hour = tablefill.hour
+            minute = request.POST.get('minutes')
+            minute = tablefill.minute
+        if auswahl == '3':
+            day = request.POST.get('days')
+            day = tablefill.dayOfMonth
+            hour = request.POST.get('hour')
+            hour = tablefill.hour
+            minute = request.POST.get('minut')
+            minute = tablefill.minute
+
+        tablefill.title = title
+        tablefill.url = url
+        tablefill.username = username
+        tablefill.password = password
+        tablefill.auth = auth
+
+        tablefill.save()
+
+        return render(request, 'Cronjob_Dev/cron.html')
+    else:
+        return render(request, 'Cronjob_Dev/cron.html')
